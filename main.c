93c3f7e0edb7e049faa1a7243fa7136a2e67a39e
Yahya Ait iazza <yait-iaz@e1r9p3.1337.ma>
2022-04-03 23:47:30 +0000

    after adding the struct

--- main.c
+++ main.c
@@ -6,12 +6,42 @@
 /*   By: yait-iaz <yait-iaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 18:58:51 by yait-iaz          #+#    #+#             */
-/*   Updated: 2022/03/28 13:24:22 by yait-iaz         ###   ########.fr       */
+/*   Updated: 2022/04/03 22:57:01 by yait-iaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philosopher.h>
 
+void	first_philo(t_philo *philo)
+{
+	philo = malloc(sizeof(t_philo));
+	if (!philo)
+		return ;
+	philo->n = 1;
+	philo->next = NULL;
+	philo->left_fork = 0;
+	philo->right_fork = 0;
+	philo->
+}
+
+void	philo_init(t_philo *philo)
+{
+	if (philo == NULL)
+		first_philo(philo);
+	philo = malloc(sizeof(t_philo));
+	philo->
+}
+
 int main(int ac, char **av)
 {
+	t_philo	*philo;
+	int		n;
+
+	n = 4;
+	philo = NULL;
+	while (n != 0)
+	{
+		philo_init(philo);
+		n--;
+	}
 }
--- philosopher.h
+++ philosopher.h
@@ -6,7 +6,7 @@
 /*   By: yait-iaz <yait-iaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 18:58:20 by yait-iaz          #+#    #+#             */
-/*   Updated: 2022/03/23 12:12:48 by yait-iaz         ###   ########.fr       */
+/*   Updated: 2022/04/03 17:10:10 by yait-iaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+
+typedef struct d_philo
+{
+	pthread_t		tread_id;
+	int				n;
+	int				eat;
+	int				think;
+	int				sleep;
+	int				left_fork;
+	int				right_fork;
+	struct d_philo	*next;
+}				t_philo;
 
 #endif
